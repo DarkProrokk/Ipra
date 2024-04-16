928e10817fa2bbbae4b58db54656951abbfa044a
GolikovVI <GolikovVI@miac.medkhv.ru>
2024-04-16 12:15:43 +1000

    Добавлен UserService

---
+++ ServiceLayer/UserService/QueryObject/UsersListDtoFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.UserService.QueryObject
+{
+    public static class UsersListDtoFilter
+    {
+        public static 
+    }
+}
---
+++ ServiceLayer/UserService/QueryObject/UsersListDtoSelect.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Entities;
+using ServiceLayer.IpraService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.UserService.QueryObject
+{
+    public static class UsersListDtoSelect
+    {
+        public static IQueryable<UsersListDto> MapUsersToDto(this IQueryable<User> users)
+        {
+            return users.Select(item => new UsersListDto
+            {
+                Id = item.UserId,
+                UserName = item.UserName,
+                Surname = item.Surname,
+                Name = item.Name,
+                Patronymic = item.Patronymic,
+                Post = item.Post,
+                UserMOUsers = item.UserMOUsers,
+                UserRoleUsers = item.UserRoleUsers
+            });
+        }
+    }
+}
