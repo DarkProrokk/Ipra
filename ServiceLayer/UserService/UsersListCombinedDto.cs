c855c3fd86642bf3697d3b97a96800a42cde965e
TheHarmont <Golikovvladislav2000@gmail.com>
2024-04-26 10:47:48 +1000

    Перенес объявление заголовков столбцов в UsersListCombinad

--- ServiceLayer/UserService/UsersListCombinedDto.cs
+++ ServiceLayer/UserService/UsersListCombinedDto.cs
@@ -14,5 +14,21 @@ namespace ServiceLayer.UserService
         public SortFilterPageOptions SortFilterPageData { get; private set; } = sortFilterPageData;
 
         public IEnumerable<UsersListDto> UsersList { get; private set; } = usersList;
+
+        public Dictionary<string, string> HeaderList { get; private set; } = GetUsersColumnHeader();
+        public static Dictionary<string, string> GetUsersColumnHeader()
+        {
+            return new Dictionary<string, string>
+            {
+                { "id", "id" },
+                { "userName", "Логин" },
+                { "userRoleUsers", "Роли" },
+                { "surname", "Фамилия" },
+                { "name", "Имя" },
+                { "patronymic", "Отчество" },
+                { "post", "Должность" },
+                { "userMOUsers", "МО" },
+            };
+        }
     }
 }
