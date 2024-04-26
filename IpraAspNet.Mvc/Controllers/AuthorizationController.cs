8c06446a89f6c8290e170caafb4ede3bc1d0c3e0
TheHarmont <Golikovvladislav2000@gmail.com>
2024-04-26 10:58:33 +1000

    Изменение логики работы сервиса авторизации

--- IpraAspNet.Mvc/Controllers/AuthorizationController.cs
+++ IpraAspNet.Mvc/Controllers/AuthorizationController.cs
@@ -10,7 +10,6 @@ using System.Security.Claims;
 
 namespace IpraAspNet.Mvc.Controllers
 {
-    //todo комментарии + docstring
     public class AuthorizationController : Controller
     {
         private readonly IUserService _userService;
@@ -37,25 +36,19 @@ namespace IpraAspNet.Mvc.Controllers
             {
                 try
                 {
-                    var login = _ldapService.GetAdUserLogin(model);
-                    var user = await _userService.GetUserAsync(login);
-                    //Todo: Вынести клаймсы в отдельный метод
-                    #region Claims
-                    var userClaims = new List<Claim>
+                    if(_ldapService.AdExist(model))
                     {
-                        new Claim("username", user.UserName),
-                        new Claim(ClaimsIdentity.DefaultRoleClaimType, user.UserRoleUsers.FirstOrDefault().Role.RoleName.ToString())
-                    };
+                        var user = await _userService.GetUserAsync(model.Login);
+                        
+                        await HttpContext.SignInAsync("CookieAuthentication", _ldapService.GetClaimsPrincipal(user));
 
-                    var principal = new ClaimsPrincipal(new ClaimsIdentity(userClaims, _ldapService.GetType().Name));
-                    #endregion
-                    await HttpContext.SignInAsync("CookieAuthentication", principal);
-
-                    return RedirectToAction("Index", "Admin");
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    ModelState.AddModelError("User was not found in AD", "Неверный логин или пароль");
+                    return View(model);
                 }
-                catch (LdapException ex)
+                catch (LdapException)
                 {
-                    var sd = ex.ErrorCode;
                     ModelState.AddModelError("incorrect login or password", "Неверный логин или пароль");
                     return View(model);
                 }
--- ServiceLayer/AuthenticationService/Concrete/LdapAuthenticationService.cs
+++ ServiceLayer/AuthenticationService/Concrete/LdapAuthenticationService.cs
@@ -5,6 +5,7 @@ using ServiceLayer.AuthorizeService.Interface;
 using ServiceLayer.UserService.Interface;
 using System.DirectoryServices.Protocols;
 using System.Net;
+using System.Security.Claims;
 
 namespace ServiceLayer.AuthorizeService.Concrete
 {
@@ -21,30 +22,53 @@ namespace ServiceLayer.AuthorizeService.Concrete
             _ldapConfig = config.Value;
         }
 
-        public string GetAdUserLogin(AuthenticationModel person)
+        public bool AdExist(AuthenticationModel person)
         {
-            //Todo docstring + комментарии
-            using (LdapConnection connection = new LdapConnection(new LdapDirectoryIdentifier(_ldapConfig.Url)))
+            try
             {
-                connection.Bind(new NetworkCredential(person.Login, person.Password));
-
-                var searchFilter = string.Format(_ldapConfig.SearchFilter, person.Login);
-
-                SearchRequest searchRequest = new SearchRequest(
-                _ldapConfig.SearchBase,
-                searchFilter,
-                SearchScope.Subtree,
-                new[] { MemberOfAttribute, DisplayNameAttribute, SAMAccountNameAttribute, "company" });
-
-                SearchResponse searchResponse = (SearchResponse)connection.SendRequest(searchRequest);
-
-                if (searchResponse.Entries.Count == 1)
+                // Установка соединения с сервером AD
+                using (LdapConnection connection = new LdapConnection(new LdapDirectoryIdentifier(_ldapConfig.Url)))
                 {
-                    return searchResponse.Entries[0].Attributes["sAMAccountName"][0].ToString();
+                    //Проверка соответствия данных в AD
+                    connection.Bind(new NetworkCredential(person.Login, person.Password));
+
+                    var searchFilter = string.Format(_ldapConfig.SearchFilter, person.Login);
+
+                    SearchRequest searchRequest = new SearchRequest(
+                    _ldapConfig.SearchBase,
+                    searchFilter,
+                    SearchScope.Subtree,
+                    new[] { MemberOfAttribute, DisplayNameAttribute, SAMAccountNameAttribute, "company" });
+
+                    // Отправка поискового запроса на сервер LDAP с использованием соединения и получение результатов поиска
+                    SearchResponse searchResponse = (SearchResponse)connection.SendRequest(searchRequest);
+
+                    // Проверяем количество найденых персон
+                    if (searchResponse.Entries.Count == 1)
+                        return true;
+                    else
+                        return false;
                 }
-                //Todo возвращать ошибку, вместо null
-                return null;
             }
+            catch (Exception ex)
+            {
+                // TODO: Добавить логирование
+                return false;
+            }
+        }
+
+        public ClaimsPrincipal GetClaimsPrincipal(User user)
+        {
+            // Создание списка утверждений для пользователя
+            var userClaims = new List<Claim>
+            {
+                new Claim("username", user.UserName),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.UserRoleUsers.FirstOrDefault()?.Role.RoleName.ToString() ?? string.Empty)
+            };
+
+            // Создание объекта ClaimsPrincipal на основе списка утверждений и указанного типа аутентификации "ldapService"
+            var principal = new ClaimsPrincipal(new ClaimsIdentity(userClaims, "ldapService"));
+            return principal;
         }
     }
 }
